a0892840cfcecbfa9681d3f2dda34c10a2081fac
Scrabman <majestico62@aol.com>
2021-06-01 14:34:00 +0200

    Quelque modifs
    Creation de Categorie et Retrait

--- src/main/java/fr/eni/encheres/dal/DAOFactory.java
+++ src/main/java/fr/eni/encheres/dal/DAOFactory.java
@@ -2,7 +2,7 @@ package fr.eni.encheres.dal;
 
 public abstract class DAOFactory {
 	
-	public static ArticleVenduDAO getAvisDAO()
+	public static ArticleVenduDAO getArticleVenduDAO()
 	{
 		return new ArticleVenduDAOJdbcImpl();
 	}
