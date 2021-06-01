bbff6b961e1141e1a0236d26490f096da9c182ea
Silence502 <mickaelvidal51@gmail.com>
2021-06-01 13:36:58 +0200

    Ajout JSP
    Pages d'accueil et d'inscription provisoires

---
+++ src/main/java/fr/eni/encheres/servlets/ServletHomePage.java
@@ -0,0 +1,39 @@
+package fr.eni.encheres.servlets;
+
+import java.io.IOException;
+
+import jakarta.servlet.RequestDispatcher;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class ServletHomePage
+ */
+@WebServlet(description = "Servlet gérant les requêtes de la page d'accueil", urlPatterns = { "/ServletHomePage" })
+public class ServletHomePage extends HttpServlet {
+    private static final long serialVersionUID = 1L;
+
+    /**
+     * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+     *      response)
+     */
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+	    throws ServletException, IOException {
+	RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/signup.jsp");
+	rd.forward(request, response);
+    }
+
+    /**
+     * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
+     *      response)
+     */
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+	    throws ServletException, IOException {
+	// TODO Auto-generated method stub
+	doGet(request, response);
+    }
+
+}
