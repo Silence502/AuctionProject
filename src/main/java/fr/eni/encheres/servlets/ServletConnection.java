04e19f685f3fbe122150261d85dee986d7e8ca95
Silence502 <mickaelvidal51@gmail.com>
2021-06-04 00:48:44 +0200

    Màj message d'erreur lors de mauvaise saisie pour la connexion

--- src/main/java/fr/eni/encheres/servlets/ServletConnection.java
+++ src/main/java/fr/eni/encheres/servlets/ServletConnection.java
@@ -18,53 +18,54 @@ import jakarta.servlet.http.HttpSession;
  */
 @WebServlet("/ServletConnection")
 public class ServletConnection extends HttpServlet {
-    private static final long serialVersionUID = 1L;
+	private static final long serialVersionUID = 1L;
 
-    /**
-     * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
-     *      response)
-     */
-    protected void doGet(HttpServletRequest request, HttpServletResponse response)
-	    throws ServletException, IOException {
-	
-	HttpSession session = request.getSession(false);
-	
-	if (session != null) {
-	    session.invalidate();
-	}
-	RequestDispatcher rd = request.getRequestDispatcher("/home.jsp");
-	rd.forward(request, response);
-    }
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 
-    /**
-     * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
-     *      response)
-     */
-    protected void doPost(HttpServletRequest request, HttpServletResponse response)
-	    throws ServletException, IOException {
-	HttpSession session = request.getSession();
-	
-	//Récupération des champs du formulaire d'inscription
-	String pseudo = request.getParameter("pseudo");
-	String motDePasse = request.getParameter("motDePasse");
-	int isCorrect = 0;
-	
-	//Récupération et reconstruction de l'utilisateur depuis la BDD
-	UtilisateurManager utilisateurManager = new UtilisateurManager();
-	Utilisateur utilisateur = utilisateurManager.selectUtilisateur(pseudo, motDePasse);
-	request.setAttribute("user", utilisateur);
-	request.setAttribute("isCorrect", isCorrect);
+		HttpSession session = request.getSession(false);
 
-	if (utilisateur != null) {
-	    //Si le constructeur à récupéré toutes les données
-	    session.setAttribute("userSession", utilisateur);
-	    response.sendRedirect("home.jsp");
-	} else {
-	    //Si le constructeur n'a récupéré aucune données
-	    response.sendRedirect("signin.jsp");
-	    isCorrect = 1;
+		if (session != null) {
+			session.invalidate();
+		}
+		RequestDispatcher rd = request.getRequestDispatcher("/home.jsp");
+		rd.forward(request, response);
 	}
 
-    }
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		HttpSession session = request.getSession();
+
+		// Récupération des champs du formulaire d'inscription
+		String pseudo = request.getParameter("pseudo");
+		String motDePasse = request.getParameter("motDePasse");
+		boolean isCorrect = true;
+
+		// Récupération et reconstruction de l'utilisateur depuis la BDD
+		UtilisateurManager utilisateurManager = new UtilisateurManager();
+		Utilisateur utilisateur = utilisateurManager.selectUtilisateur(pseudo, motDePasse);
+		request.setAttribute("user", utilisateur);
+		request.setAttribute("isCorrect", isCorrect);
+		
+		if (utilisateur != null) {
+			// Si le constructeur à récupéré toutes les données
+			session.setAttribute("userSession", utilisateur);
+			response.sendRedirect("home.jsp");
+		} else {
+			// Si le constructeur n'a récupéré aucune données
+			response.sendRedirect("signin.jsp");
+			isCorrect = false;
+			session.setAttribute("isCorrectSession", isCorrect);
+		}
+
+	}
 
 }
